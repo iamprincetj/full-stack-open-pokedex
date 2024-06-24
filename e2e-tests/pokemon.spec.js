da77504055d4fc1328be28137b89e3686f17c2b8
iamprincetj <tochukwunwanze5@gmail.com>
2024-06-24 22:18:51 +0100

    e2e test updated

--- e2e-tests/pokemon.spec.js
+++ e2e-tests/pokemon.spec.js
@@ -1,13 +1,22 @@
-const { test, describe, expect } = require('@playwright/test')
+const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('/')
+  })
+
+  test('front page can be opened', async ({ page }) => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(
       page.getByText(
         'Pokémon and Pokémon character names are trademarks of Nintendo.'
       )
     ).toBeVisible()
+  })
+
+  test('one can navigate to another page', async ({ page }) => {
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('overgrow')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
