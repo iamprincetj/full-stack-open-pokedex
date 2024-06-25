d86a45a5f4d92cb54f41c2b5619a392c3e545f0a
iamprincetj <tochukwunwanze5@gmail.com>
2024-06-25 16:54:08 +0100

    broken code wont't deploy

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('Second time to work')
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw 'error...  '
+  res.send('ok')
 })
 
 app.listen(PORT, () => {
