f2618d786a1bfdb9b1518c540e9cb8bbc1986086
iamprincetj <tochukwunwanze5@gmail.com>
2024-06-25 16:48:29 +0100

    added health check path to render

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  res.send('ok')
+  res.send('Second time to work')
 })
 
 app.listen(PORT, () => {
